bb09de9f1a3469299bebbb0f3b67af2f7a962611
Rodrigo Caetano <rodrigofcaetano14@gmail.com>
2026-09-06 20:35:51 -0300

    teste

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel, str, Field
+from pydantic import BaseModel, Field
 
 from app.models.enums import UserRole
 from app.schemas.common import ORMModel
